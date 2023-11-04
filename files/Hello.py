91ee685db252aef00674010e6d09612745558a2e
Luis Casado <Luiscasado620@gmail.com>
2023-11-04 00:30:42 -0400

    Some python for fun

---
+++ files/Hello.py
@@ -0,0 +1,7 @@
+print("Hello Pyhon! It has been a while since the last time we spoke. I hope you're doing well mate")
+
+def Myfunction():
+  print("Functions are great for coders")
+
+
+Myfunction()
